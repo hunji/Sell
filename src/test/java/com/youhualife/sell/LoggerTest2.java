3f885e6676a50801f9a646c637964f6756ef1966
hunji <guo194194@gmail.com>
2018-01-22 17:59:28 +0800

    初始化项目

---
+++ src/test/java/com/youhualife/sell/LoggerTest2.java
@@ -0,0 +1,24 @@
+package com.youhualife.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * created by guo19
+ * created on 2018/1/22
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest2 {
+    @Test
+    public void test1(){
+        log.debug("debug...");
+        log.info("info...");
+        log.error("error...");
+        
+    }
+}
